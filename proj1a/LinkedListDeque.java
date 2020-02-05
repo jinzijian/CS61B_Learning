448d0c82189f84e8aa8dd81f98765980e106c5f8
AlexKim <zijianjin0730@gmail.com>
2020-02-05 23:27:24 +0800

    submit

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,120 +1,85 @@
 public class LinkedListDeque<T> {
-    private  class StuffNode {
-        public T item;
-        public StuffNode next;
-        public StuffNode prev;
+	private StuffNode sentinelF;
+	private StuffNode sentinelL;
+	private int size;
+	
+	private class StuffNode {
+		public StuffNode prev;
+		public T item;
+		public StuffNode next;
 
+		public StuffNode(T i, StuffNode m, StuffNode n){
+			item = i;
+			prev = m;
+			next = n;
+		}
 
-        public StuffNode(T i, StuffNode p,StuffNode n) {
-            item = i;
-            next = n;
-            prev = p;
-        }
-    }
-    private StuffNode sentinel;
-    private int size;
+	}
+	public T LinkedListDeque() {
+		sentinelF = new StuffNode( -1, null, sentinelL);
+		sentinelL = new StuffNode( -1, sentinelF, null);
+		size = 0;
+	}
+	public void addFirst (T item) {
+		StuffNode tmp = sentinelF.next;
+		sentinelF.next = new StuffNode( item, sentinelF, tmp);
+		tmp.prev = sentinelF.next;
+		size += 1;
+	}
+	public void addLast(T item) {
+		StuffNode tmp = sentinelL.prev;
+		tmp.next = new StuffNode( item, tmp, sentinelL);
+		size += 1;
+	}
+	public boolean isEmpty() {
+		if ( size!= 0 ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public int size() {
+		return size;
+	}
+	public void printDeque() {
+		StuffNode cur = sentinelF.next;
+		for (int i = 0; i < size; i++) {
+			System.out.print(cur.item + " ");
+			cur = cur.next;
+		}
+		System.out.println();
 
-    public LinkedListDeque(){
-        sentinel = new StuffNode(null,null ,null);
-        sentinel.prev = sentinel;
-        sentinel.next = sentinel;
-        /**构建双向循环链表的哨兵结点*/
-        size = 0;
-    }
-    public int size(){
-        return size;
-    }
-    public boolean isEmpty() {
-        return size == 0;
-    }
+	}
+	public T removeFirst() {
+		StuffNode tmp = sentinelF.next.next;
+		sentinelF.next = tmp;
+		tmp.prev = sentinelF;
+		size -= 1;
 
-    public void addFirst(T x) {
-        sentinel.next = new StuffNode(x,sentinel,sentinel.next);
-        sentinel.next.next.prev = sentinel.next;
-        /**prev需要被修改，next直接沿用就好*/
-        size += 1;
-    }
-    public void addLast(T x) {
-        sentinel.prev.prev.next = sentinel.prev;
-        sentinel.prev = new StuffNode(x,sentinel.prev.prev,sentinel.prev.next);/**头结点连接到新的last*/
-        /**把之前的尾结点和新的last链接*/
-        size += 1;
-    }
-    public T removeFirst() {
-        T removed = sentinel.next.item;
-        sentinel.next.next.prev = sentinel;
-        sentinel.next = sentinel.next.next;
-        if (!isEmpty()) {
-            size -= 1;
-        }
-        return removed;
-    }
-    /**
-     * Print the items in the deque from first to last, separated by a space
-     * Once all the items have been printed, print out a new line
-     */
-    public void printDeque(){
-        int i = 0;
-        StuffNode printed = sentinel.next;
-        while(i<size){
-            System.out.println(printed.item+" ");
-            printed = printed.next;
-            i+=1;
-        }
-        System.out.println();
-    }
-    public T removeLast(){
-        if (size == 0){
-            return null;
-        }
-        sentinel.prev = sentinel.prev.prev;
-        sentinel.prev.next = sentinel;
-        return sentinel.prev.item;
-    }
-    public T get(int index){
-        if(size<index){
-            return null;
-        }
-        int i = 0;
-        StuffNode geted = sentinel.next;
-        while(i<index){
-            geted = geted.next;
-            i+=1;
-        }
-        return geted.item;
-    }
-    public LinkedListDeque(LinkedListDeque other) {
-        sentinel = new StuffNode(null, null, null);
-        sentinel.prev = sentinel;
-        sentinel.next = sentinel;
-        size = 0;
-
-        for (int i = 0; i < other.size(); i++) {
-            addLast((T) other.get(i)); // (T) is cast, since type of other is unknown
-        }
-    }
-    private T getRecursive(int index, StuffNode curr){
-        if (size<index){
-            return null;
-        }
-        if (index == 0){
-            return curr.item;
-        }
-        return getRecursive(index - 1, curr.next);
-
-        }
-    }
-    public T getRecursive(int index){
-        if (size<index){
-            return null;
-        }
-        if (index == 0){
-            return sentinel.next.item;
-        }
-        if ( index < size() || size>0){
-            this.removeFirst();
-            return this.getRecursive(index -1);
-        }
-    }
+	}
+	public T removeLast() {
+		StuffNode tmp = sentinelL.prev.prev;
+		tmp.next = sentinelL;
+		sentinelL.prev = tmp;
+		size -= 1;
+	}
+	public T get(int index) {
+		StuffNode tmp = sentinelF.next;
+		int i = 0;
+		if(index > size-1 ){
+			return null;
+		}
+		while(i < index) {
+			tmp = tmp.next;
+			i += 1;
+		}
+		return tmp.item;
+	}
+	public LinkedListDeque(LinkedListDeque other) {
+		sentinelF = new StuffNode( -1, null, sentinelL);
+		sentinelL = new StuffNode( -1, sentinelF, null);
+		size = 0;
+		for (int i = 0; i < other.size(); i++) {
+			addLast(other.get(i));
+	}
 }
-
